8000a71115a08730595bd601faab3f2ab93b4e40
Your Name <kshitijkhandelwal112@gmail.com>
2026-09-18 15:03:18 +0530

    adding new solution

---
+++ 9-195A.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c; cin>>a>>b>>c;
+    cout<<(c*(a-b)+b-1)/b<<endl;
+    return 0;
+}
+// Idk who rated this problem 1000, it was very easy, must not be rated above 800.
