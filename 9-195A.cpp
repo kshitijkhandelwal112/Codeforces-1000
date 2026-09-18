44c3a9534a479a7710222ce812326832b729d8e3
Your Name <kshitijkhandelwal112@gmail.com>
2026-09-18 15:07:12 +0530

    added minor comments

--- 9-195A.cpp
+++ 9-195A.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 using namespace std;
-
 int main() {
     int a,b,c; cin>>a>>b>>c;
-    cout<<(c*(a-b)+b-1)/b<<endl;
+    cout<<(c*(a-b)+b-1)/b<<endl; // This is nothing but ceiling function, applied using +b-1
     return 0;
 }
 // Idk who rated this problem 1000, it was very easy, must not be rated above 800.
